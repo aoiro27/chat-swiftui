d9d7ae98238abd7a630fb4e393d4b4a5a242edd4
yuki.u <aoiro.savant@gmail.com>
2024-12-22 05:25:31 +0900

    チャットビューのメッセージエリア、入力エリア、ナビゲーションエリアをリファクタリングして、コードの可読性を向上

--- chat-swiftui/ChatView.swift
+++ chat-swiftui/ChatView.swift
@@ -11,47 +11,62 @@ struct ChatView: View {
     var body: some View {
         VStack(spacing:0) {
             
-            
             // Message Area
-            ScrollView {
-                VStack(spacing:0) {
-                    ForEach(0..<20) { _ in
-                        HStack {
-                            Circle().frame(width: 60, height: 60).foregroundColor(.red)
-                            Capsule().frame(height: 60).foregroundColor(.blue)
-                        }.padding(.bottom)
-                    }
-                }.padding(.horizontal)
-                    .padding(.top, 72)
-            }.background(.cyan).overlay(
-                // Navigation Area
-                HStack {
-                    Circle().frame(width: 40, height: 40)
-                    Text("タイトル")
-                    Spacer()
-                    Circle().frame(width: 40, height: 40)
-                    Circle().frame(width: 40, height: 40)
-                    Circle().frame(width: 40, height: 40)
-                }
-                    .foregroundColor(.white)
-                    .padding()
-                    .background(.black.opacity(0.5))
-                , alignment: .top
-            )
+            messageArea
             
             // Input Area
-            HStack {
-                Circle().frame(width: 40, height: 40).foregroundColor(.red)
-                Circle().frame(width: 40, height: 40).foregroundColor(.red)
-                Circle().frame(width: 40, height: 40).foregroundColor(.red)
-                Capsule().frame(height: 40).foregroundColor(.blue)
-                Circle().frame(width: 40, height: 40).foregroundColor(.red)
-            }.background(.white)
-                .padding()
+            inputArea
         }
     }
 }
 
 #Preview {
     ChatView()
+}
+
+extension ChatView {
+    private var messageArea: some View {
+        // Message Area
+        ScrollView {
+            VStack(spacing:0) {
+                ForEach(0..<20) { _ in
+                    HStack {
+                        Circle().frame(width: 60, height: 60).foregroundColor(.red)
+                        Capsule().frame(height: 60).foregroundColor(.blue)
+                    }.padding(.bottom)
+                }
+            }.padding(.horizontal)
+                .padding(.top, 72)
+        }.background(.cyan).overlay(
+            navigationArea, alignment: .top
+        )
+    }
+    
+    private var inputArea: some View {
+        // Input Area
+        HStack {
+            Circle().frame(width: 40, height: 40).foregroundColor(.red)
+            Circle().frame(width: 40, height: 40).foregroundColor(.red)
+            Circle().frame(width: 40, height: 40).foregroundColor(.red)
+            Capsule().frame(height: 40).foregroundColor(.blue)
+            Circle().frame(width: 40, height: 40).foregroundColor(.red)
+        }.background(.white)
+            .padding()
+    }
+    
+    private var navigationArea: some View {
+        // Navigation Area
+        HStack {
+            Circle().frame(width: 40, height: 40)
+            Text("タイトル")
+            Spacer()
+            Circle().frame(width: 40, height: 40)
+            Circle().frame(width: 40, height: 40)
+            Circle().frame(width: 40, height: 40)
+        }
+            .foregroundColor(.white)
+            .padding()
+            .background(.black.opacity(0.5))
+    }
+    
 }
